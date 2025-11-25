ad00de44c16bc558284bf7b12fe5731330c87249
Ilias Elbarnoussi <90471760+IliasElbarnoussi@users.noreply.github.com>
2025-11-25 15:59:09 +0100

    use new project in personal expo account for testing

--- apps/easypid/app.config.js
+++ apps/easypid/app.config.js
@@ -37,7 +37,7 @@ const APP_CONFIGS = {
     bundleId: 'id.paradym.wallet',
     additionalInvitationSchemes: ['didcomm'],
     associatedDomains: ['paradym.id', 'dev.paradym.id'],
-    // projectId: 'b5f457fa-bcab-4c6e-8092-8cdf1239027a',
+    projectId: '08b84a8d-2d07-4b32-b878-34b320eb4de6',
     assets: ['./assets/paradym/icon.png'],
     extraConfig: {
       mediatorDid: mediatorDids[process.env.APP_VARIANT || 'production'],
